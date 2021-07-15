27c3ca0f71e09555437a34cd46150ee011121ba0
maychaza <mayalejandrr@gmail.com>
2021-07-15 20:24:48 -0300

    Agrego los atributos de InfoPreguntaNueva, se agrega metodo en PreguntaService y en PreguntaController se crea el @PostMapping

--- src/main/java/ar/com/ada/api/questionados/controllers/PreguntaController.java
+++ src/main/java/ar/com/ada/api/questionados/controllers/PreguntaController.java
@@ -6,9 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import ar.com.ada.api.questionados.entities.Pregunta;
+import ar.com.ada.api.questionados.models.request.InfoPreguntaNueva;
+import ar.com.ada.api.questionados.models.response.GenericResponse;
 import ar.com.ada.api.questionados.services.PreguntaService;
 
 @RestController
@@ -17,6 +21,20 @@ public class PreguntaController {
     @Autowired
     PreguntaService service;
 
+    @PostMapping("/preguntas")
+    public ResponseEntity<?> crearPregunta(@RequestBody InfoPreguntaNueva preguntaNueva){
+
+        GenericResponse respuesta = new GenericResponse();
+
+        Pregunta pregunta = service.crearPregunta(preguntaNueva.enunciado, preguntaNueva.opciones, preguntaNueva.categoria);
+
+        respuesta.isOk = true;
+        respuesta.id = preguntaNueva.getPreguntaPorId();
+        respuesta.message = "LA pregunta fue creada con éxito";
+
+        return ResponseEntity.ok(preguntaNueva);
+    }
+
     @GetMapping("/categorias")
     public ResponseEntity<List<Pregunta>> traerPreguntas(){
         return ResponseEntity.ok(service.traerPreguntas());
@@ -24,7 +42,7 @@ public class PreguntaController {
 
     @GetMapping("/categorias/{id}")
     public ResponseEntity<?> getPreguntaPorId(@PathVariable Integer id){
-        
+
         Pregunta pregunta = service.buscarEmpleadaPorId(id);
         
         if (pregunta == null){
--- src/main/java/ar/com/ada/api/questionados/services/PreguntaService.java
+++ src/main/java/ar/com/ada/api/questionados/services/PreguntaService.java
@@ -6,7 +6,9 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import ar.com.ada.api.questionados.entities.Categoria;
 import ar.com.ada.api.questionados.entities.Pregunta;
+import ar.com.ada.api.questionados.entities.Respuesta;
 import ar.com.ada.api.questionados.repos.PreguntaRepository;
 
 @Service
@@ -15,11 +17,14 @@ public class PreguntaService {
     @Autowired
     PreguntaRepository repo;
 
+    @Autowired
+    CategoriaService categoriaService;
+
     public List<Pregunta> traerPreguntas(){
         return repo.findAll();
     }
 
-    public Pregunta buscarEmpleadaPorId(Integer PreguntaId){
+    public Pregunta buscarPreguntaPorId(Integer PreguntaId){
 
         Optional<Pregunta> resultado = repo.findById(PreguntaId);   // findbyId: busca por Id una pregunta, te devuelve 1 optional<pregunta>
 
@@ -28,4 +33,20 @@ public class PreguntaService {
         return null;     
     }
     
+    public Pregunta crearPregunta(String enunciado, Integer categoriaId, List<Respuesta> opciones){
+        
+        Pregunta pregunta = new Pregunta();
+        pregunta.setEnunciado(enunciado);
+
+        Categoria categoria = categoriaService.buscarCategoria(categoriaId);
+        
+        pregunta.setCategoria(categoria);
+        
+        for (Respuesta respuesta: opciones){   //se ejecuta, se llama a recorrer la lista y a c/ respuesta se le setea la preg
+            respuesta.setPregunta(pregunta);
+        }
+        
+        repo.save(pregunta);
+        return pregunta;
+    }
 }
