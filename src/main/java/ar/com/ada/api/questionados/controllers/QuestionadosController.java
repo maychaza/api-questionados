b08a76937ddf36119468cf666b3b754b9a6a923d
maychaza <mayalejandrr@gmail.com>
2021-07-20 00:03:22 -0300

    Se hacen los ultimos metodos que devuelve una proxima pregunta y verifica si una respuesta dada esta ok

--- src/main/java/ar/com/ada/api/questionados/controllers/QuestionadosController.java
+++ src/main/java/ar/com/ada/api/questionados/controllers/QuestionadosController.java
@@ -1,16 +1,48 @@
 package ar.com.ada.api.questionados.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import ar.com.ada.api.questionados.entities.Pregunta;
+import ar.com.ada.api.questionados.entities.Respuesta;
+import ar.com.ada.api.questionados.models.request.RespuestaAVerificar;
+import ar.com.ada.api.questionados.models.response.RespuestaVerificada;
+import ar.com.ada.api.questionados.services.QuestionadosService;
 
 @RestController
 public class QuestionadosController {
 
-    //GET/questionados/preguntas
+   @Autowired
+   QuestionadosService service;
 
     //GET/ questionados/next
+    @GetMapping("/questionados/next") //si no hay variables y es un GET, no lleva nada dentro de los parentesis.
+    public ResponseEntity<Pregunta> traerPreguntaRandom(){
+        
+        Pregunta proximaPregunta = service.traerPreguntaRandom();
+        
+        return ResponseEntity.ok(proximaPregunta);
 
-    //GET/ questionados/preguntas/categoria/{catId}/next*/
+        //return ResponseEntity.ok(service.traerPreguntaRandom());    forma sintetizada
+    }
 
+    @PostMapping("/questionados/verificaciones")
+    public ResponseEntity<RespuestaVerificada> verificarRespuesta(@RequestBody RespuestaAVerificar respuestaAVerificar){
+
+        RespuestaVerificada respuestaVerificada = new RespuestaVerificada();
+        if (service.verificarRespuesta(respuestaAVerificar.preguntaId, respuestaAVerificar.respuestaId)){
+            respuestaVerificada.esCorrecta = true;
+        } else {
+            respuestaVerificada.esCorrecta = false;
+        }
+
+        return ResponseEntity.ok(respuestaVerificada);
+    }
     
     
 }
